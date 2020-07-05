8c19abdfb6caa6fae1f59442ead7262db88ebdc1
Landmaster3375 <52403447+masudattouteki15@users.noreply.github.com>
2020-07-06 02:53:14 +0900

    Add files via upload

---
+++ 20200706_GameStarter_withCheatSheet.bat
@@ -0,0 +1,4 @@
+@echo off
+START "" "C:\Users\%username%\AppData\Local\NVIDIA Corporation\GeForceNOW\CEF\GeForceNOW.exe"
+REM START C:\Users\%username%\Dropbox\ゲーム用チートシート\20200706_Satisfactoryチートシート1.jpg
+REM START C:\Users\%username%\Dropbox\ゲーム用チートシート\20200706_Satisfactoryチートシート2.jpg
