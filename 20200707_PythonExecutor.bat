dff03dbff5154de8ce6de9b8a8d47e687d76ed3e
Landmaster3375 <52403447+masudattouteki15@users.noreply.github.com>
2020-07-07 21:23:32 +0900

    Update 20200707_PythonExecutor.bat

--- 20200707_PythonExecutor.bat
+++ 20200707_PythonExecutor.bat
@@ -4,7 +4,7 @@ REM --- code in utf-8 ---
 REM Drag .py file to me.
 
 for %%i in (%*) do (
-REM Comment one of two following lines out.
+REM Comment out one of two following lines.
 REM C:\Users\%username%\AppData\Local\Continuum\anaconda3\python.exe %%i
 REM C:\Users\OCT--\Anaconda3\python.exe %%i
 )
