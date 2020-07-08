7dd3e7e3548613706c80eafc10d5615e3e50ad08
Landmaster3375 <52403447+masudattouteki15@users.noreply.github.com>
2020-07-08 16:54:43 +0900

    Update 20200707_PythonExecutor.bat

--- 20200707_PythonExecutor.bat
+++ 20200707_PythonExecutor.bat
@@ -6,6 +6,6 @@ REM Drag .py file to me.
 for %%i in (%*) do (
 REM Comment out one of two following lines.
 REM C:\Users\%username%\AppData\Local\Continuum\anaconda3\python.exe %%i
-REM C:\Users\OCT--\Anaconda3\python.exe %%i
+REM C:\Users\%username%\Anaconda3\python.exe %%i
 )
 pause
