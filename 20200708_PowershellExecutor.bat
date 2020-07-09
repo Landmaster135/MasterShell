0341f3fb522c2cba90d0d99c3d5d9dc482bcbe7d
Landmaster3375 <52403447+masudattouteki15@users.noreply.github.com>
2020-07-09 10:13:54 +0900

    Update 20200708_PowershellExecutor.bat

--- 20200708_PowershellExecutor.bat
+++ 20200708_PowershellExecutor.bat
@@ -6,7 +6,6 @@ REM Drag .ps1 file to me.
 echo "PowershellExecutor.bat"
 
 for %%i in (%*) do (
-# powershell -NoProfile -ExecutionPolicy Unrestricted .\hogehoge.ps1
 powershell -NoProfile -ExecutionPolicy Unrestricted %%i
 )
 pause
