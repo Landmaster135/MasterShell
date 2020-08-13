f39d8ef4a1cfbf2e563f171db07f8e66849d4917
Landmaster3375 <52403447+masudattouteki15@users.noreply.github.com>
2020-08-13 16:36:06 +0900

    Create 20200813_.sh

---
+++ 20200813_.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TARGETDIR=20200813_export_test
+
+echo 'start time' >> /backup_nfs/work/$TARGETDIR/timestamp.txt
+date >> /backup_nfs/work/$TARGETDIR/timestamp.txt
+
+/usr/sap/PD3/HDB02/exe/hdbsql -U PIOAPKEY -I /backup_nfs/work/$TARGETDIR/export.sql -o /backup_nfs/work/$TARGETDIR/CONSULT_HISTORY_INFO.csv
+
+echo 'end time' >> /backup_nfs/work/$TARGETDIR/timestamp.txt
+date >> /backup_nfs/work/$TARGETDIR/timestamp.txt
