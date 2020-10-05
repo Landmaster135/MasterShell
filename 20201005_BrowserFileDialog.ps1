9f1994d5925461172cf07ff09803bc3369894bbf
Landmaster3375 <52403447+masudattouteki15@users.noreply.github.com>
2020-10-05 19:11:49 +0900

    Add files via upload

---
+++ 20201005_BrowserFileDialog.ps1
@@ -0,0 +1,26 @@
+﻿# --- code by utf-8 ---
+# BrowserFileDialog.ps1
+
+$abolutely_path = 'Desktop'
+
+function browserFileDialog{
+  param($abolutely_path)
+
+  Add-Type -AssemblyName System.Windows.Forms
+  $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+    InitialDirectory = [Environment]::GetFolderPath($abolutely_path);
+    Filter = 'text (*.txt,*.csv)|*.txt;*.csv';
+    Title = 'Select File';
+  }
+  
+  if($FileBrowser.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK){
+    # [System.Windows.Forms.MessageBox]::Show($FileBrowser.FileName)
+    return $FileBrowser.FileName;
+  }else{
+    # [System.Windows.Forms.MessageBox]::Show('Nothing selected.')
+    return $false;
+  }
+}
+
+# browse file dialog.
+browserFileDialog $abolutely_path;
