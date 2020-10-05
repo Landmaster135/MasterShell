f554c16e21c20e391b5c1d1b27d1d629d2cff35a
Landmaster3375 <52403447+masudattouteki15@users.noreply.github.com>
2020-10-05 17:24:24 +0900

    Add files via upload

---
+++ 20201005_ExtractorFromFolder.ps1
@@ -0,0 +1,13 @@
+﻿# --- code by utf-8 ---
+# ExtractorFilenameIncludingSubFolder.ps1
+
+# date of execution
+$execDate     = Get-Date -Format "yyyyMMdd";
+$execTime     = Get-Date -Format "HHmmss";
+$execDateTime = $execDate + $execTime;
+$exportFile   = 'fileList.txt';
+
+# extract file list from folder.
+Set-Location C:\Users\95066\Downloads;
+Get-ChildItem -Path * | ForEach-Object{Write-Host $_.name};
+Get-ChildItem -Path * | ForEach-Object{Write-Output $_.name >>$exportFile};
