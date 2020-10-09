033e01261f57bd278bb7ce9308cd26b112c1de7a
Landmaster3375 <52403447+masudattouteki15@users.noreply.github.com>
2020-10-09 18:13:47 +0900

    Add files via upload

---
+++ 20201009_ParallelExecutor.ps1
@@ -0,0 +1,31 @@
+﻿function Do_Process(){
+    param(
+        [string] $text
+    )
+
+    $time_wait = 3
+    Start-Sleep -Seconds $time_wait
+    Write-Host "$text, Wait:$time_wait"
+}
+
+function Test_Sync(){
+    foreach ($i in 1..5) {
+        Do_Process "Process ${i}"
+    }
+}
+
+Workflow Test_Parallel(){
+    foreach -parallel ($i in 1..5) {
+        Do_Process "Process ${i}"
+    }
+}
+
+$time_sync = Measure-Command {
+    Test_Sync
+}
+$time_parallel = Measure-Command {
+    Test_Parallel
+}
+
+$time_sync.TotalMilliseconds
+$time_parallel.TotalMilliseconds
