053c494c12c8854c5cc827c0cbae3010a04e91d2
Landmaster3375 <52403447+masudattouteki15@users.noreply.github.com>
2020-10-13 20:18:57 +0900

    Add files via upload

---
+++ 20201013_ExecuteShortcutURL.bat
@@ -0,0 +1,9 @@
+@echo off
+
+:: --- code in utf-8 ---
+:: Extension is not ".lnk" but ".url" in case link is for URL.
+:: Execute lnk file.
+start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Github_Web.url"
+start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Gmail_Web.url"
+start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google_Drive.url"
+
