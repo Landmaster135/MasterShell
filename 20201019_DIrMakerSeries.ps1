2972a1675dfe85e65b35106d9b0889d11d3bce3d
Landmaster3375 <52403447+masudattouteki15@users.noreply.github.com>
2020-10-19 17:29:26 +0900

    Add files via upload

---
+++ 20201019_DIrMakerSeries.ps1
@@ -0,0 +1,48 @@
+﻿function ReceiverInputtedKey(){
+  param(
+    [string[]] $choices
+  );
+
+  $nowDir = pwd;
+  $choices_joined = $choices -join "／";
+  $inputQuestion = "Your Directory is "+ $nowDir + ". You Make Directory? [" + $choices_joined + "]";
+  $inputAnswer = "";
+
+  while ($choices -cnotcontains $inputAnswer) {
+    $inputAnswer = Read-Host $inputQuestion;
+  }
+
+  return $inputAnswer;
+}
+
+function MakeDirSeries(){
+  param(
+    [string] $targetDir,
+    [System.Object[]] $numList
+  );
+  
+  $choices = "Y", "n";
+  $inputAnswer = (ReceiverInputtedKey $choices);
+
+  if ($inputAnswer -ceq $choices[0]){
+    Write-Output "Making directories now.....";
+  } elseif ($inputAnswer -ceq $choices[1]) {
+    exit;
+  } else {
+    Write-Host "Unexpected Key Error. Exit.";
+    exit;
+  }
+  
+  $numList | ForEach-Object {
+    # .ToString()メソッド100の位までゼロ埋め
+    # （10の位までなら'00'を指定）
+    $dirName = $_.ToString('000');
+
+    New-Item -ItemType Directory "No.$dirName";
+  }
+}
+
+$targetDir = "C:\Users\95066\Downloads";
+$numList   = 0..100;
+MakeDirSeries $targetDir $numList;
+
---
+++ 20201019_UserPostNumAPI.ps1
@@ -0,0 +1,29 @@
+﻿5..8 | % {
+
+  # URLの生成（例として日本郵便が公開する【郵便番号API】へのリクエストURLを作成）
+  $url = "https://api.zipaddress.net/?zipcode=904000$_"
+
+  # HTTPリクエストの送信
+  Write-Host "テスト実行（URL：${url}）"
+  $res = Invoke-WebRequest $url
+
+  # HTTPレスポンスの応答本文のJSONをオブジェクトに変換する
+  $obj = ConvertFrom-Json $res.Content
+
+  # テスト結果を出力
+  if ( $obj.code -eq '200' ) {
+    Write-Host 'コード値正常' -ForegroundColor Cyan
+    Write-Host ('住所：' + $obj.data.fullAddress) -ForegroundColor Cyan
+
+  } elseif ( $obj.code -eq '404' ) {
+    Write-Host ('コード値異常：' + $obj.code ) -ForegroundColor Red
+    Write-Host '住所が存在しません' -ForegroundColor Red
+
+  } else {
+    Write-Host ('コード値異常：' + $obj.code ) -ForegroundColor DarkRed
+  }
+
+  # 次のURLをテストする前に2秒開ける（郵便番号APIサーバへの負荷防止）
+  Start-Sleep -Seconds 2
+}
+
