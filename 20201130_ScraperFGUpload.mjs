60523b2bd434837244d85f211a1d508e5c242fc8
Landmaster3375 <52403447+masudattouteki15@users.noreply.github.com>
2020-11-30 17:27:28 +0900

    Update 20201130_ScraperFGUpload.mjs

--- 20201130_ScraperFGUpload.mjs
+++ 20201130_ScraperFGUpload.mjs
@@ -13,17 +13,29 @@ var data = [];
 // });
 // console.log(data);
 
+// This statement can't be realized, too.
+// var reader = new FileReader();
+// array.forEach(function(value) {
+//     console.log([...document.querySelectorAll('td')][value].innerText);
+// });
+// console.log(data[0],data[1],data[2],data[3],data[4],data[5]);
 
+// This statement makes space.
+// var reader = new FileReader();
+// array.forEach(function(value) {
+//     console.log([...document.querySelectorAll('td')][value].innerText);
+// });
+// half_length_of_data = data.length/2
+// var sep = ",";
+// console.log(data[0+half_length_of_data], sep, data[1+half_length_of_data], sep, data[2+half_length_of_data], sep, data[3+half_length_of_data], sep, data[4+half_length_of_data], sep, data[5+half_length_of_data]);
+
+
+// this statement can realize.
 var reader = new FileReader();
 array.forEach(function(value) {
     console.log([...document.querySelectorAll('td')][value].innerText);
 });
-console.log(data);
-
-// select area to copy text.
-document.getSelection().selectAllChildren(data);
-// copy text to get information.
-document.execCommand("copy");
-// cancel area selection.
-document.getSelection().empty(target);
+half_length_of_data = data.length/2
+var sep = ",";
+console.log(`${data[0+half_length_of_data]}${sep}${data[1+half_length_of_data]}${sep}${data[2+half_length_of_data]}${sep}${data[3+half_length_of_data]}${sep}${data[4+half_length_of_data]}${sep}${data[5+half_length_of_data]}`);
 
