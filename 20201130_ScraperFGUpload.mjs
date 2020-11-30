9d9d402dd1e0d294a51ed8321e67d808f33342fd
Landmaster3375 <52403447+masudattouteki15@users.noreply.github.com>
2020-11-30 18:11:22 +0900

    Update 20201130_ScraperFGUpload.mjs

--- 20201130_ScraperFGUpload.mjs
+++ 20201130_ScraperFGUpload.mjs
@@ -25,20 +25,20 @@ var data = [];
 // array.forEach(function(value) {
 //     data.push([...document.querySelectorAll('td')][value].innerText);
 // });
-// half_length_of_data = data.length/2 - 1
+// half_length_of_data = 0
 // var sep = ",";
 // console.log(data[0+half_length_of_data], sep, data[1+half_length_of_data], sep, data[2+half_length_of_data], sep, data[3+half_length_of_data], sep, data[4+half_length_of_data], sep, data[5+half_length_of_data]);
 
 
 // this statement can realize.
 var reader = new FileReader();
-var array = [324,326,328,330,334,338];
+var array = [338,324,326,328,334,330];
 var info;
 var data = [];
 array.forEach(function(value) {
     data.push([...document.querySelectorAll('td')][value].innerText);
 });
-half_length_of_data = data.length/2 - 1
+var length_of_data = 0
 var sep = ",";
-console.log(`${data[0+half_length_of_data]}${sep}${data[1+half_length_of_data]}${sep}${data[2+half_length_of_data]}${sep}${data[3+half_length_of_data]}${sep}${data[4+half_length_of_data]}${sep}${data[5+half_length_of_data]}`);
+console.log(`${data[0+length_of_data]}${sep}${data[1+length_of_data]}${sep}${data[2+length_of_data]}${sep}${data[3+length_of_data]}${sep}${data[4+length_of_data]}${sep}${data[5+length_of_data]}`);
 
