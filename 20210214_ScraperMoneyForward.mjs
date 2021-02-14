9f7991db231dbc8c27c38e1f30c06ac80240dae7
Landmaster135 <52403447+masudattouteki15@users.noreply.github.com>
2021-02-15 03:38:34 +0900

    Update 20210214_ScraperMoneyForward.mjs

--- 20210214_ScraperMoneyForward.mjs
+++ 20210214_ScraperMoneyForward.mjs
@@ -6,25 +6,30 @@
 var reader = new FileReader();
 
 var array_expense_fixed_month = ['食費 合計', '日用品 合計', '水道・光熱費 合計', '通信費 合計', '住宅 合計'];
-var array_expense_fixed_year = ['交通費 合計', '衣服・美容 合計', '健康・医療 合計', '特別な支出 合計', '税・社会保障 合計'];
+var array_expense_fixed_year  = ['交通費 合計', '衣服・美容 合計', '健康・医療 合計', '特別な支出 合計', '税・社会保障 合計'];
 var array_expense_extra_month = ['趣味・娯楽 合計', '教養・教育 合計'];
-var array_expense_extra_year = ['現金・カード 合計', 'その他 合計', '未分類 合計'];
+var array_expense_extra_year  = ['交際費 合計', '自動車 合計', '保険 合計'];
+var array_expense_others      = ['現金・カード 合計', 'その他 合計', '未分類 合計'];
 var str_expense;
 var array_result_fixed_month_category = [];
-var array_result_fixed_year_category = [];
+var array_result_fixed_year_category  = [];
 var array_result_extra_month_category = [];
-var array_result_extra_year_category = [];
+var array_result_extra_year_category  = [];
+var array_result_others_category      = [];
 var array_result_fixed_month = [];
-var array_result_fixed_year = [];
+var array_result_fixed_year  = [];
 var array_result_extra_month = [];
-var array_result_extra_year = [];
+var array_result_extra_year  = [];
+var array_result_others      = [];
+
 var i = 0;
 
 do {
     i = iCounter_and_pushDataInConditions(array_expense_fixed_month, array_result_fixed_month_category, array_result_fixed_month, i);
-    i = iCounter_and_pushDataInConditions(array_expense_fixed_year, array_result_fixed_year_category, array_result_fixed_year, i);
+    i = iCounter_and_pushDataInConditions(array_expense_fixed_year , array_result_fixed_year_category , array_result_fixed_year, i);
     i = iCounter_and_pushDataInConditions(array_expense_extra_month, array_result_extra_month_category, array_result_extra_month, i);
-    i = iCounter_and_pushDataInConditions(array_expense_extra_year, array_result_extra_year_category, array_result_extra_year, i);
+    i = iCounter_and_pushDataInConditions(array_expense_extra_year , array_result_extra_year_category , array_result_extra_year, i);
+    i = iCounter_and_pushDataInConditions(array_expense_others     , array_result_others_category     , array_result_others, i);
     i++;
 }while(`${[...document.querySelectorAll('td')][i].innerText}` != '20');
 
@@ -37,6 +42,8 @@ console.log(outputTextFromArray(array_result_extra_month_category, "\t", ""));
 console.log(outputTextFromArray(array_result_extra_month, "\n", ""));
 console.log(outputTextFromArray(array_result_extra_year_category, "\t", ""));
 console.log(outputTextFromArray(array_result_extra_year, "\n", ""));
+console.log(outputTextFromArray(array_result_others_category, "\t", ""));
+console.log(outputTextFromArray(array_result_others, "\n", ""));
 
 function pullOutExcrescence(str_target) {
     var str_return;
