eae04ff100032d33130e103ce1969b7b144362ce
kinkinbeer135ml <52403447+Landmaster135@users.noreply.github.com>
2021-06-01 16:59:44 +0900

    Update 20210601_WBTterminator.js

--- 20210601_WBTterminator.js
+++ 20210601_WBTterminator.js
@@ -1,5 +1,13 @@
 // innerHTML："\n\n  <b>1/10</b>\n"
+console.log(document.getElementById("PAGE").innerHTML);
 var pageNum = Number(document.getElementById("PAGE").innerHTML.substr(9,2));
+for(var i = 0; i < pageNum; i++){
+	goNext();
+}
+
+// innerHTML："<b>1/10</b>"
+console.log(document.getElementById("PAGE").innerHTML);
+var pageNum = Number(document.getElementById("PAGE").innerHTML.substr(5,2));
 for(var i = 0; i < pageNum; i++){
 	goNext();
 }
