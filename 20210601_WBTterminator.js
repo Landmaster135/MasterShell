d73374b4148e872d3f2bf070138089abefb1f372
kinkinbeer135ml <52403447+Landmaster135@users.noreply.github.com>
2021-06-25 10:12:39 +0900

    Update 20210601_WBTterminator.js

--- 20210601_WBTterminator.js
+++ 20210601_WBTterminator.js
@@ -13,6 +13,7 @@
 // innerHTML："\n\n  <b>1/10</b>\n"
 console.log(document.getElementById("PAGE").innerHTML);
 var pageNum = Number(document.getElementById("PAGE").innerHTML.substr(9,2));
+// pageNum = 30; // 動かなかったらこっちで。作った側としては無念だが。
 for(var i = 0; i < pageNum; i++){
 	goNext();
 }
